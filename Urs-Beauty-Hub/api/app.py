6dc9bb7fb63b9a9e88e9371587f903d73a29885c
Siva Mykel <sivavenkat.aj@gmail.com>
2025-10-05 05:09:00 +0530

    first commit

---
+++ Urs-Beauty-Hub/api/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # optional, allows cross-origin requests
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+
+
